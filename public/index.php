0c43e1b36c8d75f59922a2af9be478aca774c764
trey-causey <trey.causey@outlook.com>
2020-11-17 15:57:02 -0500

    decided where to put models directory and working on configuration changes

--- public/index.php
+++ public/index.php
@@ -7,8 +7,11 @@ error_reporting(E_ALL);
 
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/app');
+define('MODEL_DIR', BASE_PATH . '/src/models');
 
 try {
+    //adding autoloader since factory didn't
+    require_once BASE_PATH . '/vendor/autoload.php';
     /**
      * The FactoryDefault Dependency Injector automatically registers
      * the services that provide a full stack framework.
